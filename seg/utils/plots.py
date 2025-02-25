fc5bdd1cf0ffcbc40e253e2efc99ab86a1556e35
Jiang, Rong <ronjian@github.com>
2025-02-25 16:08:36 +0800

    minor

--- seg/utils/plots.py
+++ seg/utils/plots.py
@@ -87,7 +87,9 @@ class Annotator:
         if self.pil or not is_ascii(label):
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
             if label:
-                w, h = self.font.getsize(label)  # text width, height
+                bbox = self.font.getbbox(label)
+                w = bbox[2] - bbox[0]  # 右边界 - 左边界
+                h = bbox[3] - bbox[1]  # 下边界 - 上边界
                 outside = box[1] - h >= 0  # label fits outside box
                 self.draw.rectangle(
                     (box[0], box[1] - h if outside else box[1], box[0] + w + 1,
@@ -120,7 +122,8 @@ class Annotator:
     def text(self, xy, text, txt_color=(255, 255, 255), anchor='top'):
         # Add text to image (PIL-only)
         if anchor == 'bottom':  # start y from font bottom
-            w, h = self.font.getsize(text)  # text width, height
+            bbox = self.font.getbbox(text)
+            w, h = bbox[2] - bbox[0], bbox[3] - bbox[1] 
             xy[1] += 1 - h
         self.draw.text(xy, text, fill=txt_color, font=self.font)
 
